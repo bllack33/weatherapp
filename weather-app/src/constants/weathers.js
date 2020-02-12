8297e0d12a6ce954646ce009801e0ed8ffd894c5
bllack33 <bllack33@gmail.com>
2020-02-11 23:18:20 -0500

    20200211

---
+++ weather-app/src/constants/weathers.js
@@ -0,0 +1,11 @@
+export const CLOUD = 'cloud';
+
+export const CLOUDY = 'cloudy';
+
+export const SUN = 'sun';
+
+export const RAIN = 'rais';
+
+export const SNOW = 'snow';
+
+export const WINDY = 'windy';
